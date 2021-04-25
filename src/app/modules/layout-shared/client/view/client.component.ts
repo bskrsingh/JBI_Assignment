2f7881ec16afaaea6bfe40d693a98acb7bd4c09b
unknown <bskrsingh@gmail.com>
2021-04-25 10:23:21 +0530

    changes for client work

--- src/app/modules/layout-shared/client/view/client.component.ts
+++ src/app/modules/layout-shared/client/view/client.component.ts
@@ -6,7 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client.component.scss']
 })
 export class ClientComponent implements OnInit {
-
+  clientData = [
+    {
+      clientName: "TUI GROUP",
+      img : '../assets/images/clients/1.jpg',
+      clientWork: 'Lorem ipsum dolor sit amet, consectetur adipisicing elittio Lorem ipsum dolor sit amet, consectetur adipisicing elittio.'
+    },
+    {
+      clientName: "TUI GROUP",
+      img : '../assets/images/clients/2.jpg',
+      clientWork: 'Lorem ipsum dolor sit amet, consectetur adipisicing elittio Lorem ipsum dolor sit amet, consectetur adipisicing elittio.'
+    },
+    {
+      clientName: "TUI GROUP",
+      img : '../assets/images/clients/3.jpg',
+      clientWork: 'Lorem ipsum dolor sit amet, consectetur adipisicing elittio Lorem ipsum dolor sit amet, consectetur adipisicing elittio.'
+    },
+    {
+      clientName: "TUI GROUP",
+      img : '../assets/images/clients/4.jpg',
+      clientWork: 'Lorem ipsum dolor sit amet, consectetur adipisicing elittio Lorem ipsum dolor sit amet, consectetur adipisicing elittio.'
+    },
+    {
+      clientName: "TUI GROUP",
+      img : '../assets/images/clients/5.jpg',
+      clientWork: 'Lorem ipsum dolor sit amet, consectetur adipisicing elittio Lorem ipsum dolor sit amet, consectetur adipisicing elittio.'
+    },
+    {
+      clientName: "TUI GROUP",
+      img : '../assets/images/clients/6.jpg',
+      clientWork: 'Lorem ipsum dolor sit amet, consectetur adipisicing elittio Lorem ipsum dolor sit amet, consectetur adipisicing elittio.'
+    }
+  ]; 
   constructor() { }
 
   ngOnInit(): void {
